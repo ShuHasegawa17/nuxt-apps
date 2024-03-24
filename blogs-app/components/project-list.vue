71265ab9d306c64ef2ae91a54acec4f85cc9c159
shuhasegawa <shuhasegawa0430@gmail.com>
2024-03-24 17:59:29 +0900

    fetched GigHub Project

--- blogs-app/components/project-list.vue
+++ blogs-app/components/project-list.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+const { error, pending, data } = await useFetch<any>(
+  'https://api.github.com/users/ShuHasegawa17/repos'
+);
+console.log(data.value);
+const repos = computed(() =>
+  data.value
+    //.filter((repo: { description: any }) => repo.description)
+    .sort((a: any, b: any) => b.stargazers_count - a.stargazers_count)
+);
+</script>
+
 <template>
-  <div>projects</div>
+  <p class="mb-10">Take a look at my Gihub Projects!</p>
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">somthing went wrong,,, Try again!</section>
+  <section v-else>
+    <ul class="grid grid-cols-1 gap-4">
+      <li
+        v-for="repsitory in repos"
+        :key="repsitory.id"
+        class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+      >
+        <a :href="repsitory.html_url" target="_blank">
+          <div class="flex item-center justify-between text-sm">
+            <div class="font-semibold">{{ repsitory.name }}</div>
+            <div>{{ repsitory.stargazers_count }} *</div>
+          </div>
+          <p class="text-sm">
+            {{ repsitory.description }}
+          </p>
+        </a>
+      </li>
+    </ul>
+  </section>
 </template>
--- blogs-app/layouts/default.vue
+++ blogs-app/layouts/default.vue
@@ -8,7 +8,7 @@
       </div>
       <Menu />
     </header>
-    <main class="p-2">
+    <main class="p-2 mt-10">
       <slot />
     </main>
   </div>
