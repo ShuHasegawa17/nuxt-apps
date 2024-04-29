9ea7005ff73e90a9a30abc973f369fdc23f2ca78
shuhasegawa <shuhasegawa0430@gmail.com>
2024-04-29 11:17:17 +0900

    env setting

--- finance-tracker/pages/login.vue
+++ finance-tracker/pages/login.vue
@@ -41,6 +41,7 @@ const email = ref('');
 const pending = ref(false);
 const { toastError } = useAppToast();
 const supabase = useSupabaseClient();
+const redirectUrl = useRuntimeConfig().public.baseUrl;
 
 useRedirectIfAuthenticated();
 
@@ -50,7 +51,7 @@ const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email.value,
       options: {
-        emailRedirectTo: 'http://localhost:3000/confirm',
+        emailRedirectTo: `${redirectUrl}/confirm`,
       },
     });
     if (error) {
